d9e561cf8e4b9254c2745944f2438c454064ad19
김동기 <voicek@gimdong-giui-MacBookAir.local>
2022-11-15 21:56:46 +0900

    ADD:bestProduct page 구성중

--- src/pages/BestProductList/BestProductList.js
+++ src/pages/BestProductList/BestProductList.js
@@ -1,10 +1,134 @@
 import React from "react";
-
-const BestProductList = () => {
-  return(
-    <div>BestProductList</div>
-  )
+import "../BestProductList/BestProductList.scss";
+function BestProductList() {
+  return (
+    <div className="bestProduct">
+      <div className="leftSide">
+        <div className="leftCategories"></div>
+        <h2 className="title">BEST</h2>
+        <ul className="leftSideList">
+          <ul className="categryNameList">
+            <li className="categryName">
+              <a className="firstName" href="#">
+                여성의류
+              </a>
+            </li>
+            <li className="categoryName">
+              <a className="secondeName" href="#">
+                남성의류
+              </a>
+            </li>
+            <li className="categoryName">
+              <a className="thirdName" href="#">
+                신발
+              </a>
+            </li>
+          </ul>
+        </ul>
+      </div>
+      <div className="mainPage">
+        <h2 className="kindOfThing">여성의류</h2>
+        <div className="categories">
+          <ul className="categoriesList">
+            <span className="list">
+              <input
+                className="kind"
+                type="radio"
+                name="categoryMediumList"
+                value
+                checked
+              />
+              <label
+                className="choice"
+                for="categoryMediumList"
+                title="categoryMediumList"
+              >
+                전체
+              </label>
+            </span>
+            <span className="list">
+              <input
+                className="kind"
+                type="radio"
+                name="secondCategoryMediumList"
+                value="secondCategoryMediumList"
+              />
+              <label
+                className="choice"
+                for="secondCategoryMediumList"
+                title="categoryMediumList"
+              >
+                상의
+              </label>
+            </span>
+            <span className="list">
+              <input
+                className="kind"
+                type="radio"
+                name="thirdCategoryMediumList"
+                value="thirdCategoryMediumList"
+              />
+              <label
+                className="choice"
+                for="thirdCategoryMediumList"
+                title="categoryMediumList"
+              >
+                하의
+              </label>
+            </span>
+          </ul>
+        </div>
+        <ul className="productList">
+          <li className="product">
+            <div className="ware">
+              <a href="#" title="제품정보" className="productInfo">
+                <div className="wareInfo">
+                  <img src="/images/kimdongki/woman.jpg" alt="woman" />
+                </div>
+              </a>
+              <div className="productExplanation">
+                <a href="#" title="브랜드" className="brand">
+                  브랜드
+                </a>
+                <a href="#" title="제품이름" className="wareName">
+                  <div className="wareShowing">
+                    <h5 className="wareExplanation">제품이름_black</h5>
+                    <div className="price">
+                      <strong className="beforeDiscount">485,000</strong>
+                      <div className="discount">
+                        <span className="percent">5 %</span>
+                        <strong className="discountPrice">460,750</strong>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <div className="like">
+                  <button className="heart">
+                    <img
+                      src="/images/kimdongki/heart.svg"
+                      width={16}
+                      height={16}
+                      viewBox="0 0 18 18"
+                    ></img>
+                    9,184
+                  </button>
+                  <a href="#" className="comment">
+                    <img
+                      src="/images/kimdongki/comment.svg"
+                      width={16}
+                      height={16}
+                      viewBox="0 0 15 16"
+                    ></img>
+                    <span className="review">644</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default BestProductList;
-
