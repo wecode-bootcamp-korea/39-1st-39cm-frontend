8f20d56c397801cef8547c3cddd84b327f0c1410
Da-bin, Lee <110847597+Dabnii@users.noreply.github.com>
2022-11-15 16:45:50 +0900

    ADD: 39cm Nav layout (SCSS) 구축완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,93 @@
-import React from 'react';
+import React from "react";
+import "./Nav.scss";
 
-function Nav() {
+export default function Nav() {
   return (
-    <div>
-      nav
-    </div>
+    <>
+      <nav className="Nav">
+        <section className="navContent">
+          <img
+            className="Logo"
+            src="/images/leedabin/BlackLogo.png"
+            alt="39cm logo"
+          />
+          <div className="navContentBox">
+            <div className="mypage">
+              <img
+                className="mypageIcon"
+                src="/images/leedabin/mypage.png"
+                alt="MY PAGE"
+              />
+              <span>MY PAGE</span>
+            </div>
+            <div className="mylike">
+              <img
+                className="heartIcon"
+                src="/images/leedabin/heart.png"
+                alt="MY LIKE"
+              />
+              <span>MY LIKE</span>
+            </div>
+            <div className="shoppingBag">
+              <img
+                className="shoppingIcon"
+                src="/images/leedabin/shoppingbag.png"
+                alt="SHOPPING BAG"
+              />
+              <span>SHOPPING BAG</span>
+            </div>
+            <div className="login">
+              <img
+                className="loginIcon"
+                src="/images/leedabin/login.png"
+                alt="LOGIN"
+              />
+              <span>Login </span>
+            </div>
+          </div>
+        </section>
+        <section className="navCategoriesWrap">
+          <ul className="navCategories">
+            <li>BEST</li>
+            <li>WOMEN</li>
+            <li>MEN</li>
+            <li>UNISEX</li>
+          </ul>
+          <img
+            class="searchIcon"
+            src="/images/leedabin/search.png"
+            alt="Search"
+          />
+        </section>
+      </nav>
+      <section className="navSearch">
+        <div className="navSearch">
+          <img
+            className="searchCloseBtn"
+            src="/images/leedabin/closeBtn.png"
+            alt="closeBtn"
+          />
+          <div className="sideSearchBox">
+            <div className="inputContainer">
+              <input className="navSearchInput" placeholder="Search" />
+              <img
+                className="searchIcon"
+                src="/images/leedabin/search.png"
+                alt="searchIcon"
+              />
+            </div>
+            <section className="searchLank">
+              <span className="topTopic">인기검색어</span>
+              <div className="topKeyword">
+                <ul>
+                  <li>니트</li>
+                  <li>프라이탁</li>
+                </ul>
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
-
-export default Nav;
