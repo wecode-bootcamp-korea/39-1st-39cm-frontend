d812c6afe17fa982d41741f03f1b332e49cd76f4
Da-bin, Lee <110847597+Dabnii@users.noreply.github.com>
2022-11-21 21:59:54 +0900

    ADD: carousel, 하트 그 외 SCSS 수정

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,11 +1,40 @@
-import React from "react";
-import "./ProductDetail.scss";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./ProductDetail.scss";
 
 const ProductDetail = () => {
+  const images = useRef([
+    {
+      src: "/images/leedabin/400_NB_yellow_set.jpg",
+    },
+    {
+      src: "/images/leedabin/400_NB_yellow_set_twogirls.jpg",
+    },
+    {
+      src: "/images/leedabin/400_NB_yellow_sitting.jpg",
+    },
+  ]);
+
   const [showColorOpt, setShowColorOpt] = useState(false);
   const [likePd, setLikePd] = useState(false);
+  const [showBox, setShowBox] = useState(false);
+  const [current, setCurrent] = useState(0);
+  const [style, setStyle] = useState({ marginLeft: `-${current}00%` });
+
+  const imgSize = useRef(images.current.length);
+
+  const moveSlide = (i) => {
+    let nextIndex = current + i;
+
+    if (nextIndex < 0) nextIndex = imgSize.current - 1;
+    else if (nextIndex >= imgSize.current) nextIndex = 0;
+
+    setCurrent(nextIndex);
+  };
+
+  useEffect(() => {
+    setStyle({ marginLeft: `-${current}00%` });
+  }, [current]);
 
   const hendleScrollUp = (e) => {
     if (!window.scrollY) return;
@@ -40,28 +69,27 @@ const ProductDetail = () => {
           <div className="productThumbnail">
             <div className="arrowsSet">
               <button className="arrowLeft">
-                <img src="/images/leedabin/arrowLeft.png" alt="arrowLeft" />
+                <img
+                  src="/images/leedabin/arrowLeft.png"
+                  alt="arrowLeft"
+                  onClick={() => {
+                    moveSlide(-1);
+                  }}
+                />
               </button>
-              <button className="arrowRight">
+              <button
+                className="arrowRight"
+                onClick={() => {
+                  moveSlide(1);
+                }}
+              >
                 <img src="/images/leedabin/arrowRight.png" alt="arrowright" />
               </button>
             </div>
-            <div className="flexBox">
-              <img
-                className="nbThn1"
-                src="/images/leedabin/NB_yellow_set.jpeg"
-                alt="thumbnail"
-              />
-              <img
-                className="nbThn2"
-                src="/images/leedabin/NB_yellow_set_twogirls.jpeg"
-                alt="thumbnail2"
-              />
-              <img
-                className="nbThn3"
-                src="/images/leedabin/NB_yellow_sitting.jpeg"
-                alt="thumbnail3"
-              />
+            <div className="flexBox" style={style}>
+              {images.current.map((img, i) => (
+                <img key={i} className="img" src={img.src} alt="thumbnail" />
+              ))}
             </div>
           </div>
           <section className="productDetailBox">
@@ -104,7 +132,7 @@ const ProductDetail = () => {
                   className="heartLineIcon"
                   alt="heart"
                   onClick={() => {
-                    setLikePd(!likePd);
+                    setLikePd((prev) => !prev);
                   }}
                   src={
                     likePd === false
@@ -118,12 +146,12 @@ const ProductDetail = () => {
               297,000
               <span>원</span>
             </h2>
-            <section className="ColorWrapper" data-role="selectbox">
+            <section className="colorWrapper" data-role="selectbox">
               <section className="selectBox">
                 <button
                   type="button"
                   className="toggleBtn"
-                  onClick={() => setShowColorOpt(!showColorOpt)}
+                  onClick={() => setShowColorOpt((prev) => !prev)}
                 >
                   COLOR
                   <img
@@ -135,7 +163,7 @@ const ProductDetail = () => {
                 {showColorOpt && (
                   <ul
                     className="selectBoxOption"
-                    onClick={() => setShowColorOpt(false)}
+                    onClick={() => setShowBox((prev) => !prev)}
                   >
                     <li>
                       <button type="button" className="optionBtn">
@@ -156,6 +184,27 @@ const ProductDetail = () => {
                 )}
               </section>
             </section>
+            {showBox && (
+              <section className="itemSelectContainer">
+                <div className="itemOptBox">
+                  <span className="itemOptName">BLACK</span>
+                  <div className="optBtnContainer">
+                    <button className="addOptMinus">-</button>
+                    <button className="addOptNum">1</button>
+                    <button className="addOptAdd">+</button>
+                  </div>
+                  <span className="itemOptAll">297,000원</span>
+                  <span className="deleteItem">X</span>
+                </div>
+                <div className="itemOptBottom">
+                  <span className="finalPriceKor">총 상품 금액</span>
+                  <span className="finalPriceWon">
+                    297,000<span className="wonKor">원</span>
+                  </span>
+                </div>
+              </section>
+            )}
+
             <div className="orderBtns">
               <Link to={"/Cart"}>
                 <button className="addCartBtn" type="button">
@@ -225,7 +274,10 @@ const ProductDetail = () => {
         </div>
         <section className="reviewBox">
           <div className="reviewPhoto">
-            <img src="/images/leedabin/NB_yellow_set.jpeg" alt="reviewPhoto" />
+            <img
+              src="/images/leedabin/NB_yellow_sitting.jpg"
+              alt="reviewPhoto"
+            />
           </div>
         </section>
         <div className="reviewCtx">
