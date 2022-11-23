b4684a9964e9341870b29ba7be84c4dec5e38b32
Da-bin, Lee <110847597+Dabnii@users.noreply.github.com>
2022-11-24 01:19:06 +0900

    Modified: SCSS

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,18 +1,7 @@
 import React from "react";
-import "./MyPage.scss";
 
 const MyPage = () => {
-  return (
-    <div className="Mypage">
-      <div className="itemList">
-        <div className="columnName">
-          <span className="itemInfoCol">상품 정보</span>
-          <span className="quantityCol">수량</span>
-          <span className="orderPriceCol">주문금액</span>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>MyPage</div>;
 };
 
 export default MyPage;
